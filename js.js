4fcc217ce24e4d5430a7e98010cdc00129e1d22b
erdaserhan <erhan209@gmail.com>
2024-10-17 14:27:08 +0200

    form added

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
-let retString = localStorage.getItem("quantity");
-let retArray = JSON.parse(retString)
+
 
 fetch('products.json')
 .then(response => response.json())
@@ -12,7 +11,7 @@ fetch('products.json')
                 <img src="${product.image}">
                 <h3>${product.name}</h3>
                 <h4>${product.description}<h4>
-                <p>${product.price} ${retArray.name}</p>
+                <p>${product.price}</p>
                 <p>Quantity : ${product.quantity}</p>
                 <button onclick="addToCart('${product.name}', ${product.price})">Add to Cart</button>
             </div>
@@ -20,7 +19,6 @@ fetch('products.json')
         findDiv.appendChild(newElement)
     }    
 })
-
 
 
 let Items = [];
@@ -52,5 +50,42 @@ function deleteFromCart(index) {
 function updateQuantity(index, quantity) {
     Items[index].quantity = quantity;
     updateCartDisplay();
+    localStorage.setItem("quantity", JSON.stringify(Items));
 }
 
+function checkout() {
+    let totalPrice = 0;
+    Items.forEach(item => {
+        totalPrice += item.price * item.quantity;
+    });
+    window.location.pathname = "/form.html"
+}
+
+function updateCartDisplay() {
+    let retString = localStorage.getItem("quantity");
+    let retArray = JSON.parse(retString)
+
+    const cartElement = document.getElementById('cart-items');
+    const cartElementTotal = document.getElementById('cart-items-total');
+    cartElement.innerHTML = '';
+    Items.forEach((item, index) => {
+        const li = document.createElement('li');
+        li.className = 'cart-item';
+        li.innerHTML = `
+            <span>${item.name} - ${(item.price*item.quantity).toFixed(2)}
+            <div class="quantity">
+                <button onclick="updateQuantity(${index},${item.quantity - 1})">-</button>
+                <input type="number" value="${item.quantity}" min="0" max="10" onchange="updateQuantity(${index}, this.value)">
+                <button onclick="updateQuantity(${index}, ${item.quantity + 1})">+</button>
+            </div>
+            </span>
+            <button onclick="deleteFromCart(${index})">Delete</button>
+        `;
+        let totalPrice = 0;
+        Items.forEach(item => {
+        totalPrice += item.price * item.quantity;
+        cartElementTotal.innerHTML = `Total : ${totalPrice.toFixed(2)}`
+    });
+        cartElement.appendChild(li);        
+    });
+}
